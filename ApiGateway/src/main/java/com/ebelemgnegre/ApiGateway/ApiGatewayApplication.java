91901f351d91f04bb8cd425cb64806794cfe9020
ebelemgnegre <ebelemgnegre@nine7soft.com>
2024-02-04 22:13:55 +0000

    add: circuit breaker resiliencef4

--- ApiGateway/src/main/java/com/ebelemgnegre/ApiGateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/ebelemgnegre/ApiGateway/ApiGatewayApplication.java
@@ -1,7 +1,15 @@
 package com.ebelemgnegre.ApiGateway;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -10,4 +18,11 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+				.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofMillis(200)).build())
+				.build());
+	}
 }
---
+++ ApiGateway/src/main/java/com/ebelemgnegre/ApiGateway/controller/FallbackController.java
@@ -0,0 +1,18 @@
+package com.ebelemgnegre.ApiGateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/userServiceFallback")
+    public String userServiceFallback(){
+        return "User service is currently unavailable. Please try again later.";
+    }
+
+    @GetMapping("/movieServiceFallback")
+    public String movieServiceFallback(){
+        return "Movie service is currently unavailable. Please try again later.";
+    }
+}
